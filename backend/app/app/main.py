d01aae8f77575898944daa25de604463073c054d
Anastasios Gogos <13665570+tgogos@users.noreply.github.com>
2021-09-29 18:01:58 +0300

    add routing to jinja templates and static files

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
+# imports for UI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+
+# static files folder
+app.mount("/static", StaticFiles(directory="/app/app/static"), name="static")
+
+# templates folder
+templates = Jinja2Templates(directory="/app/app/ui")
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
@@ -25,3 +36,9 @@ nefapi = FastAPI(title="Northbound APIs", openapi_url=f"{settings.API_V1_STR}/ne
 nefapi.include_router(nef_router, prefix=settings.API_V1_STR)
 app.mount("/nef", nefapi)
 '''
+
+# ================================= Static Page routes =================================
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
