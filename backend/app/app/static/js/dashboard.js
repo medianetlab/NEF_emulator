21006b944a1500572e4cbb1e787d617ddfc362d5
Anastasios Gogos <13665570+tgogos@users.noreply.github.com>
2021-10-07 14:26:05 +0300

    add js to update card numbers

--- backend/app/app/static/js/dashboard.js
+++ backend/app/app/static/js/dashboard.js
@@ -1,3 +1,40 @@
+var gNBs = null;
+
 $( document ).ready(function() {
+    api_get_gNBs();
     
 });
+
+
+
+
+function api_get_gNBs() {
+    
+    var url = app.api_url + '/gNBs/?skip=0&limit=100';
+
+    $.ajax({
+        type: 'GET',
+        url:  url,
+        contentType : 'application/json',
+        headers: {
+            "authorization": "Bearer " + app.auth_obj.access_token
+        },
+        processData:  false,
+        success: function(data)
+        {
+            console.log(data);
+            gNBs = data;
+            ui_update_card( '#num-gnbs-card' , gNBs.length );
+        },
+        error: function(err)
+        {
+            console.log(err);
+        },
+        timeout: 5000
+    });
+}
+
+
+function ui_update_card( element_id, number ) {
+    $( element_id ).html(number);
+}
