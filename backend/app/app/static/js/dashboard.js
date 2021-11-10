01e4ab58cfb7d1fb6f5ebf27712083bccfc55d25
Anastasios Gogos <13665570+tgogos@users.noreply.github.com>
2021-11-10 16:26:51 +0200

    add DELETE button to gNBs datatables

--- backend/app/app/static/js/dashboard.js
+++ backend/app/app/static/js/dashboard.js
@@ -6,6 +6,34 @@ var cells = null;
 var ues   = null;
 var paths = null;
 
+var gNBs_datatable = null;
+
+
+
+// templates for buttons
+// =====================
+// delete_gNB
+var del_gNB_btn_tpl = `<button class="btn btn-sm btn-outline-secondary" type="button" onclick="show_delete_gNB_modal( '{{gNB_id}}' );">
+  <svg class="icon">
+    <use xlink:href="static/vendors/@coreui/icons/svg/free.svg#cil-trash"></use>
+  </svg>
+</button>`;
+
+// edit gNB
+var edit_gNB_btn_tpl = `<button class="btn btn-sm btn-outline-dark" type="button" onclick="show_edit_gNB_modal( '{{gNB_id}}' );">
+  <svg class="icon">
+    <use xlink:href="static/vendors/@coreui/icons/svg/free.svg#cil-pencil"></use>
+  </svg>
+</button>`;
+
+// modal for delete_gNB confirmation
+var del_gNB_modal = new coreui.Modal(document.getElementById('del_gNB_modal'), {});
+var gNB_to_be_deleted = -1;
+
+
+
+
+
 
 
 
@@ -17,8 +45,22 @@ $( document ).ready(function() {
     api_get_Cells()
     api_get_UEs()
     api_get_paths()
+
+
+    // add listener to buttons
+    $('#del_gNB_btn').on('click', function(){
+        // console.log(gNB_to_be_deleted);
+        api_delete_gNB( gNB_to_be_deleted );
+        del_gNB_modal.hide();
+    });
+
+
+
 });
 // ===============================================
+
+
+
 
 
 
@@ -181,6 +223,54 @@ function api_get_paths() {
 }
 
 
+// Ajax request to delete gNB
+// on success:
+//
+// 
+function api_delete_gNB( gNB_id ) {
+    
+    var url = app.api_url + '/gNBs/' + gNB_id;
+
+    $.ajax({
+        type: 'DELETE',
+        url:  url,
+        contentType : 'application/json',
+        headers: {
+            "authorization": "Bearer " + app.auth_obj.access_token
+        },
+        processData:  false,
+        beforeSend: function() {
+            // 
+        },
+        success: function(data)
+        {
+            // console.log(data);
+            ui_display_toast_msg("success", "Success!", "The gNB has been permanently deleted");
+            
+            // find index and remove from datatable
+            var indexes = gNBs_datatable
+                .rows()
+                .indexes()
+                .filter( function ( value, index ) {
+                    return gNB_id === gNBs_datatable.row(value).data().gNB_id;
+                } );
+                
+            gNBs_datatable.rows(indexes).remove().draw();
+        },
+        error: function(err)
+        {
+            // console.log(err);
+            ui_display_toast_msg("error", "Error: gNB could not be deleted", err.responseJSON.detail);
+        },
+        complete: function()
+        {
+            // 
+        },
+        timeout: 5000
+    });
+}
+
+
 // Helper function to update the numbers displayed on every card
 // 
 function ui_update_card( element_id, number ) {
@@ -193,7 +283,7 @@ function ui_update_card( element_id, number ) {
 //               Datatable functions
 // ===============================================
 function ui_init_datatable_gNBs() {
-    $('#dt-gNBs').DataTable( {
+    gNBs_datatable = $('#dt-gNBs').DataTable( {
         data: gNBs,
         responsive: true,
         paging: false,
@@ -201,12 +291,27 @@ function ui_init_datatable_gNBs() {
         info: false,
         pageLength: -1,
         lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        columnDefs: [
+            {
+                "targets": 5,
+                "data": null,
+                "defaultContent": '',
+                "orderable" : false,
+                "searchable": false,
+                "render": function ( data, type, row ) {
+                    // return row.id;
+                    return  (  del_gNB_btn_tpl.replaceAll("{{gNB_id}}", row.gNB_id) + " " +
+                              edit_gNB_btn_tpl.replaceAll("{{gNB_id}}", row.gNB_id) );
+                }
+            }
+        ],
         columns: [
             { "data": "id", className: "dt-center" },
             { "data": "gNB_id", className: "dt-center" },
             { "data": "name", className: "dt-center" },
             { "data": "description" },
             { "data": "location", className: "dt-center" },
+            { "data": null, className: "dt-right" },
         ]
     } );
 }
@@ -248,4 +353,27 @@ function ui_init_datatable_UEs() {
         ]
     } );
 }
+
+
+
+function ui_display_toast_msg( type, title, text ) {
+    toastr_options = {
+        closeButton     : true,
+        positionClass   : "toast-bottom-right",
+        // timeOut         : 0,
+        // extendedTimeOut : 0
+    };
+    toastr[type](text, title, toastr_options);
+}
 // ===============================================
+
+
+
+
+
+function show_delete_gNB_modal( gNB_id ) {
+
+    gNB_to_be_deleted = gNB_id;
+    del_gNB_modal.show();
+
+}
