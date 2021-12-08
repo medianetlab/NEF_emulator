d4e3c1a4e804ac5c37c407b87d0bc268096c475e
JFrgs <jimfragos890@gmail.com>
2021-12-08 12:44:16 +0200

    Create two FastAPI applications
    1) NEF Emulator | 2) Northbound APIs

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -10,13 +10,15 @@ api_router.include_router(paths.router, prefix="/paths", tags=["Paths"])
 api_router.include_router(gNB.router, prefix="/gNBs", tags=["gNBs"])
 api_router.include_router(Cell.router, prefix="/Cells", tags=["Cells"])
 api_router.include_router(UE.router, prefix="/UEs", tags=["UEs"])
-api_router.include_router(monitoringevent.router, prefix="/3gpp-monitoring-event/v1", tags=["Monitoring Event API"])
-api_router.include_router(qosMonitoring.router, prefix="/3gpp-as-session-with-qos/v1", tags=["Session With QoS API"])
-api_router.include_router(qosInformation.router, prefix="/qosInfo", tags=["QoS Information"])
+# api_router.include_router(monitoringevent.router, prefix="/3gpp-monitoring-event/v1", tags=["Monitoring Event API"])
+# api_router.include_router(qosMonitoring.router, prefix="/3gpp-as-session-with-qos/v1", tags=["Session With QoS API"])
+# api_router.include_router(qosInformation.router, prefix="/qosInfo", tags=["QoS Information"])
 #api_router.include_router(monitoringevent.monitoring_callback_router, prefix="/3gpp-monitoring-event/v1", tags=["Monitoring Event API"])
 
-'''
-    ---Trying to create a subapp---
+
+    # ---Create a subapp---
 nef_router = APIRouter()
 nef_router.include_router(monitoringevent.router, prefix="/3gpp-monitoring-event", tags=["Monitoring Event API"])
-'''
+nef_router.include_router(qosMonitoring.router, prefix="/3gpp-as-session-with-qos/v1", tags=["Session With QoS API"])
+nef_router.include_router(qosInformation.router, prefix="/qosInfo", tags=["QoS Information"])
+
