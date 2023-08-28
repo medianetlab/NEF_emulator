2427988f6aeea90bfffec53df8b1f1a20f9e3d35
JFrgs <jimfragos890@gmail.com>
2023-08-28 13:59:15 +0300

    fix make prestart.sh script

--- backend/app/prestart.sh
+++ backend/app/prestart.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
+# Create initial data in DB
+python /app/app/initial_data.py
+
 # Let the DB start
 python /app/app/backend_pre_start.py
 
 # Run migrations
 #alembic upgrade head
-
-# Create initial data in DB
-python /app/app/initial_data.py
