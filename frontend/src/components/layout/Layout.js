7e4771b74c7c821444b12ed4608a1a16097a2b4e
JerryPapada <gerasimospapd@gmail.com>
2024-04-17 13:05:50 +0300

    layout fixed --fix map not rendering issue

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import SidebarComponent from '../sidebar/SidebarComponent';
 import TopbarComponent from '../topbar/TopbarComponent';
-import CentralPanel from '../central-panel/CentralPanelComponent';
-import './Layout.css'; // Import CSS file
+import CentralPanelComponent from '../central-panel/CentralPanelComponent';
+//import LogoContainerComponent from '../logo-container/LogoContainerComponent'; // Import LogoContainer
+import './Layout.css'; 
 
 const Layout = ({ token }) => {
   return (
     <div className="layout">
-      <SidebarComponent />
-      <div className="content">
-        <TopbarComponent />
-        <CentralPanel token={token} />
+      <div className="sidebar-column">
+        <div className="logo-container">
+          {}
+        </div>
+        <div className="sidebar">
+          <SidebarComponent />
+        </div>
+      </div>
+      <div className="main-column">
+        <div className="topbar">
+          <TopbarComponent />
+        </div>
+        <div className="central-panel">
+          <CentralPanelComponent token={token} />
+        </div>
       </div>
     </div>
   );
 };
+
+
+
 
 export default Layout;
 
