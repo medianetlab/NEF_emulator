a8c28926aef22e9389c0428beb7ccbdbd5872ab3
JerryPapada <gerasimospapd@gmail.com>
2024-04-24 13:43:30 +0300

    row container with cards

--- frontend/src/components/dashboard/DashboardComponent.js
+++ frontend/src/components/dashboard/DashboardComponent.js
@@ -1,69 +1,15 @@
-// DashboardComponent.js
-
 import React, { useState, useEffect } from 'react';
-import { getUsers, getCells, getNotifications, getPaths, getGnbs } from '../../utils/api';
 import './DashboardComponent.css';
-
-// Define the RowContainer component outside of the DashboardComponent
-const RowContainer = () => {
-  const numbersFromBackend = [1, 2, 3, 4];
-  
-  return (
-    <div className="row-container">
-      {numbersFromBackend.map((number, index) => (
-        <div key={index} className="box">
-          {number}
-        </div>
-      ))}
-    </div>
-  );
-};
+import RowContainer from './RowContainer';
 
 const DashboardComponent = ({ token }) => {
-  const [users, setUsers] = useState([]);
-  const [cells, setCells] = useState([]);
-  const [notifications, setNotifications] = useState([]);
-  const [paths, setPaths] = useState([]);
-  const [gnbs, setGnbs] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  // get token from dashboard page
-  useEffect(() => {
-    fetchData();
-  }, [token]);
-
-  // fetch data from api.js
-  const fetchData = async () => {
-    try {
-      const userData = await getUsers(token);
-      setUsers(userData);
-
-      const cellsData = await getCells(token);
-      setCells(cellsData);
-
-      const notificationsData = await getNotifications(token);
-      setNotifications(notificationsData);
-
-      const pathsData = await getPaths(token);
-      setPaths(pathsData);
-
-      const gnbsData = await getGnbs(token);
-      setGnbs(gnbsData);
-
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setLoading(false);
-    }
-  };
-
   return (
     <div className="dashboard-component">
-      {/* Render the row container */}
-      <RowContainer />
+      <RowContainer token={token}/> {}
     </div>
   );
 };
 
 export default DashboardComponent;
+
 
