879e5d156d482b5859c2de68681be0b2e73106aa
JerryPapada <gerasimospapd@gmail.com>
2024-09-09 16:32:28 +0300

    socket/server attempt 2

--- frontend/src/utils/server.js
+++ frontend/src/utils/server.js
@@ -1,20 +1,16 @@
 import http from 'http';
 import WebSocket from 'ws';
-import fetch from 'node-fetch'; // Import node-fetch to make HTTP requests
+import fetch from 'node-fetch'; 
 
-// Create an HTTP server
 const server = http.createServer();
-
-// Create a WebSocket server
 const wss = new WebSocket.Server({ server });
 
-// Function to fetch data from the API and send it to clients
 const fetchDataAndUpdateClients = async () => {
     try {
         const response = await fetch('http://localhost:4443/api/v1/ue_movement/state-ues');
         const data = await response.json();
 
-        // Broadcast the data to all connected WebSocket clients
+        // Broadcast data
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify(data));
@@ -25,17 +21,11 @@ const fetchDataAndUpdateClients = async () => {
     }
 };
 
-// Check for data updates every 5 seconds
-setInterval(fetchDataAndUpdateClients, 5000);
+setInterval(fetchDataAndUpdateClients, 1000);   //1 sec
 
-// Handle WebSocket connections
 wss.on('connection', ws => {
-    console.log('New WebSocket connection');
-    
-    // Send a welcome message
-    ws.send('Welcome to the WebSocket server!');
+    console.log('New connection');
 
-    // Handle incoming messages
     ws.on('message', message => {
         console.log(`Received message: ${message}`);
         ws.send(`You said: ${message}`);
